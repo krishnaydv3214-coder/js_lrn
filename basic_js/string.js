3d5b59c002682e58299f5252533a4f8ece370f75
krishnaydv3214-coder <krishnaiiitdwd3214@gmail.com>
2026-06-01 20:01:38 +0000

    upadted

--- basic_js/string.js
+++ basic_js/string.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "krishna"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('krishna-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,11 +22,11 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   krishna    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://krishna.com/hitesh%20yadav"
 
 console.log(url.replace('%20', '-'))
 
